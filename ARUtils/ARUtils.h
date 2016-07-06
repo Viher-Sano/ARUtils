99e515814d24a52c5d756234a8fdc577c440fda2
Aleksandr Rudyk <alakai.1998.28@gmail.com>
2016-07-06 12:16:23 +0300

    Initial Commit

---
+++ ARUtils/ARUtils.h
@@ -0,0 +1,13 @@
+//
+//  ARUtils.h
+//  ARUtils
+//
+//  Created by Alexander Rudyk on 06.07.16.
+//  Copyright © 2016 Alexander Rudyk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARUtils : NSObject
+
+@end
---
+++ ARUtils/ARUtils.m
@@ -0,0 +1,13 @@
+//
+//  ARUtils.m
+//  ARUtils
+//
+//  Created by Alexander Rudyk on 06.07.16.
+//  Copyright © 2016 Alexander Rudyk. All rights reserved.
+//
+
+#import "ARUtils.h"
+
+@implementation ARUtils
+
+@end
